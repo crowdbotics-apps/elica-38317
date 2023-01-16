ce1914e194c6cf749d0ebed3235792bd6acdc88a
Crowdbotics <team@crowdbotics.com>
2023-01-16 18:30:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elica-38317",
+  url: "https://elica-38317.botics.co",
+  api: "https://elica-38317.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
